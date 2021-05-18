5d45bef8d2ede540c26de06d975139efd51a7285
Serguei Kozlenko <genesis.biconsulting@gmail.com>
2021-05-18 14:23:31 +0200

    DZ_3 Построение таблицы First Commit

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,28 @@
+import csv
+from app.settings import BASE_DIR
+
 from django.shortcuts import render
+
+
+def try_float(v):
+    try:
+        return int(v) if float(v).is_integer() else float(v)
+    except ValueError:
+        return None
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    csv.register_dialect('custom_scv', delimiter=';', quoting=csv.QUOTE_NONE)
 
-    return render(request, template_name,
-                  context)
+    with open(f"{BASE_DIR}\\inflation_russia.csv", newline='', encoding="utf-8") as csv_file:
+        reader = csv.DictReader(csv_file, dialect='custom_scv')
+        headers = reader.fieldnames
+        content = []
+        for row in reader:
+            formatted_values = [try_float(item) for item in row.values()]
+            content.append(formatted_values)
+
+    context = {'data': content, 'headers': headers}
+    return render(request, template_name, context)
